bc7c1bb701f21324d9468ef0f57be560525b9082
Maycon-alt <mayconthiago2@hotmail.com>
2023-06-08 17:41:10 -0300

    comit1

---
+++ src/pages/catalogo.js
@@ -0,0 +1,8 @@
+
+export default function Catalogo(){
+    return(
+        <>
+            <h1>Oi, aqui é o catalo</h1>
+        </>
+    )
+}
---
+++ src/pages/components/about1.js
@@ -0,0 +1,10 @@
+import Link from "next/link"
+import Image from "next/image"
+
+export default function Navbar(){
+    return(
+        <>
+            <h1>Oi, aqui é o catalo</h1>
+        </>
+    )
+}
