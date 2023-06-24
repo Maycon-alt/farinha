ff13ff9afd2109cba3e7cb800044e1638c15430b
Maycon-alt <mayconthiago2@hotmail.com>
2023-06-24 15:08:49 -0300

    ultimo_comit

--- src/pages/components/About.js
+++ src/pages/components/About.js
@@ -3,18 +3,18 @@ import Image from "next/image"
 
 export default function About(){
     return(
-        <div id="historia" className="bg-gray-600 w-full lg:h-[850px] bg-cover bg-center bg-no-repeat flex flex-col justify-end pb-10 sm:pb-32 py-32 lg:pb-[110px] pl-[50px] pr-[50px]">
+        <div id="historia" className=" w-full lg:h-[850px] bg-cover bg-center bg-no-repeat flex flex-col justify-end pb-10 sm:pb-32 py-32 lg:pb-[110px] pl-[50px] pr-[50px]">
             <h2 className="font-mono text-center text-5xl font-medium mt-2 pb-[100px]">Nossa Empresa</h2>
             {/* <h2 className="text- center text-4xl font-medium mt-2">Quem Somos</h2> */}
-            <div className="bg-red-300  flex itens-start justify-between flex-col-reverse lg:flex-row">
-                <div className="bg-green-400 w-full lg:max-w-[530px] sm:pt-10 sm:text-center lg:text-left lg:pr-10 xl:pl-25">
+            <div className="flex itens-start justify-between flex-col-reverse lg:flex-row">
+                <div className="w-full lg:max-w-[530px] sm:pt-10 sm:text-center lg:text-left lg:pr-10 xl:pl-25">
                     <p className="font-mono text-red-700">Olá, bem vindo a</p>
                     <h2 className="text-4xl font-medium mt-2">AutoElite</h2>
 
                     <p className="font-mono text-red-700 my-6 text-sm sm:text-base text-justify">A escolha premium em carros de luxo. Oferecemos uma experiência excepcional de compra, com veículos de qualidade, marcas renomadas e modelos prestigiosos. Nossa equipe qualificada está pronta para atender às suas necessidades e oferecer suporte pós-venda. Visite-nos e descubra o mundo da elegância automotiva.</p>
                 </div>
                 <div>
-                    <Image className="bg-slate-300 rounded-2xl w-full h-full" width={800} height={800} src="/images/ngc.jpg" alt="foto carro"/>
+                    <Image className="rounded-2xl w-full h-full" width={800} height={800} src="/images/ngc.jpg" alt="foto carro"/>
                 </div>
             </div>
         </div>
--- src/pages/components/Slider.js
+++ src/pages/components/Slider.js
@@ -18,18 +18,27 @@ import 'swiper/css/pagination';
 export default function Slider(){
     //const slides = [carro1, carro2, carro3, carro4, carro5];
     return(
-        <div id="historia" className="bg-blue-400 w-full lg:h-[900px] bg-cover bg-center bg-no-repeat flex flex-col justify-end pb-10 sm:pb-32 py-32 lg:pb-[110px] pl-[50px] pr-[50px]">
-            <h2 className="font-mono text-center text-5xl font-medium mt-2 pb-[50px]">Nossa Empresa</h2>
-            {/* <h2 className="text- center text-4xl font-medium mt-2">Quem Somos</h2> */}
-            <div className="container flex itens-start justify-between flex-col-reverse lg:flex-row">
-                <div className="w-full lg:max-w-[530px] lg:pr-[100px] sm:pt-10 sm:text-center lg:text-left">
-                    <p className="font-mono text-red-700">Olá, bem vindo a</p>
-                    <h2 className="text-4xl font-medium mt-2">AutoElite</h2>
-
-                    <p className="font-mono text-red-700 my-6 text-sm sm:text-base text-justify">A escolha premium em carros de luxo. Oferecemos uma experiência excepcional de compra, com veículos de qualidade, marcas renomadas e modelos prestigiosos. Nossa equipe qualificada está pronta para atender às suas necessidades e oferecer suporte pós-venda. Visite-nos e descubra o mundo da elegância automotiva.</p>
-                </div>
-
-                <Image className="rounded-2xl" width={420} height={404} src="/images/ngc.jpg" alt="foto carro"/>
+        <div id="home" className="bg-blue-300 w-full lg:h-[610px]">
+            <div className="container lg:h-[900px] lg:w-[5000px] lg:pt-3 pl-40">
+            <Swiper
+                modules={[Navigation, Pagination]}
+                navigation
+                pagination
+                loop
+            >
+                <SwiperSlide>
+                    <Image className="w-full h-[600px]" width={960} height={950} src="/images/imagem1.png" alt="fotocarro1"/>
+                </SwiperSlide>
+                <SwiperSlide>
+                    <Image className="w-full h-[600px]" width={800} height={900} src="/images/imagem2.png" alt="fotocarro2"/>
+                </SwiperSlide>
+                <SwiperSlide>
+                    <Image className="w-full h-[600px]" width={800} height={900} src="/images/imagem3.png" alt="fotocarro3"/>
+                </SwiperSlide>
+                <SwiperSlide>
+                    <Image className="w-full h-[600px]" width={800} height={900} src="/images/imagem4.png" alt="fotocarro4"/>
+                </SwiperSlide>
+            </Swiper>
             </div>
         </div>
     )
